4c1c49a6196dbfcc64c6a990bd2e9ab7fda4ad73
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-01-30 21:43:19 +0100

    Button added

--- app/src/main/java/org/mobile_development/marcellis/animalviewer/MainActivity.java
+++ app/src/main/java/org/mobile_development/marcellis/animalviewer/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,17 +20,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//
+            public void onClick(View v) {
                 Intent myIntent = new Intent(MainActivity.this, FragmentActivity.class);
                 startActivity(myIntent);
+
             }
         });
+
     }
 
     @Override
